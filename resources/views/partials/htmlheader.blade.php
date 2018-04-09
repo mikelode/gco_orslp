bbfbddd7c57c3f14e3ac4fa0419b28cc545bb010
mikelode <yos_migvel_2@hotmail.com>
2018-04-09 11:05:03 -0500

    Modificando favicon

--- resources/views/partials/htmlheader.blade.php
+++ resources/views/partials/htmlheader.blade.php
@@ -3,7 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 
     <title>{{ config('app.name', 'Symva') }}</title>
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <!-- Styles -->
 
     <link href="{{ asset('/plugins/bootstrap4/dist/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
