be3fb3c35b5343f501d9172e71c8e0e5f40a2fbb
mikelode <yos_migvel_2@hotmail.com>
2018-04-12 13:15:47 -0500

    formulario de validacion de usuario modificado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,53 +1,66 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-success">
-                <div class="panel-heading">Oficina Regional de Supervisión y Liquidación de Proyectos</div>
-                <div class="panel-body">
-                    <div class="row">
-                        <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                            {{ csrf_field() }}
-
-                            <div class="form-group{{ $errors->has('tusNickName') ? ' has-error' : '' }}">
-                                <label for="tuser" class="col-md-4 control-label">Usuario</label>
-
-                                <div class="col-md-6">
-                                    <input id="tuser" type="text" class="form-control" name="tusNickName" required autofocus>
-
-                                    @if ($errors->has('tusNickName'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('tusNickName') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Contraseña</label>
-
-                                <div class="col-md-6">
-                                    <input id="password" type="password" class="form-control" name="password" required>
-
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Ingresar
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
+<div class="limiter">
+    <div class="container-login100">
+        <div class="wrap-login100">
+            <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
+                <div class="logo-form">
+                    <img src="{{ asset('/img/logo_supervision.png') }}">
                 </div>
+                <span class="login100-form-title p-b-43">
+                    Acceso al Sistema
+                </span>
+                <div class="wrap-input100 validate-input" data-validate = "Debe ingresar los datos del usuario">
+                    <input class="input100" type="text" name="tusNickName" required autofocus>
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">Usuario</span>
+                </div>
+                <div class="wrap-input100 validate-input" data-validate="Debe ingresar su contraseña">
+                    <input class="input100" type="password" name="password" required>
+                    <span class="focus-input100"></span>
+                    <span class="label-input100">Contraseña</span>
+                </div>
+                <div class="flex-sb-m w-full p-t-3 p-b-32">
+                    @if ($errors->has('tusNickName'))
+                        <span class="form-group{{ $errors->has('tusNickName') ? ' has-error' : '' }}">
+                            <strong>El usuario y/o contraseña ingresado son incorrectos</strong>
+                        </span>
+                    @endif
+                    @if ($errors->has('password'))
+                        <span class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <strong>La contraseña ingresada es incorrecta</strong>
+                        </span>
+                    @endif
+                    <div>
+                        <a href="#" class="txt1">
+                            ¿Olvidó su contraseña?
+                        </a>
+                    </div>
+                </div>
+                <div class="container-login100-form-btn">
+                    <button type="submit" class="login100-form-btn">
+                        Ingresar
+                    </button>
+                </div>
+                <div class="text-center p-t-46 p-b-20">
+                    <span class="txt2">
+                        PUNO - 2018
+                    </span>
+                </div>
+
+                <div class="login100-form-social flex-c-m">
+                    <a href="#" class="login100-form-social-item flex-c-m bg1 m-r-5">
+                        <i class="fa fa-facebook-f" aria-hidden="true"></i>
+                    </a>
+
+                    <a href="#" class="login100-form-social-item flex-c-m bg2 m-r-5">
+                        <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                </div>
+            </form>
+            <div class="login100-more" style="background-image: url('img/gore_sede.jpg');">
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,60 +12,19 @@
     <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/util.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/main.css') }}">
 </head>
-<body>
+<body style="background-color: #666666;">
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <!--<li><a href="{ route('login') }}">Login</a></li>-->
-                            <!--<li><a href="{ route('register') }}">Register</a></li>-->
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         @yield('content')
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
+    <script src="{{ asset('/plugins/jquery/jQuery-2.1.4.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('/js/main.js') }}" type="text/javascript"></script>
 </body>
 </html>
